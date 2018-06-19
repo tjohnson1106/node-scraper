1cd491fe7414f098114de8c810d53781f1099d3f
Thomas Johnson <tjohnson1106@gmail.com>
2018-06-19 14:10:44 -0400

    created async response message function and fetch html via url function

--- src/helpers.js
+++ src/helpers.js
@@ -45,3 +45,31 @@ const fromPairsToObject = compose(
   arrayPairsToObject,
   withoutNulls
 );
+
+// Handles the request(Promise)
+
+const sendResponse = res => async request => {
+  return await request
+    .then(data => res.json({status: "succes", data}))
+    .catch(({status: code = 500}) =>
+      res.status(code).json({
+        status: "failure",
+        code,
+        message: code == 404 ? "Not found" : "Request failed"
+      })
+    );
+};
+
+/**
+ * Loads the html string returned for the given URL
+ * and sends a Cheerio parser instance of the loaded HTML
+ */
+
+const fetchHtmlFromUrl = async url => {
+  return await axios
+    .get(enforceHttpsUrl(url))
+    .then(response => cheerio.load(response.data))
+    .catch(error => {
+      error.status = (error.response && error.response.status) || 500;
+    });
+};
